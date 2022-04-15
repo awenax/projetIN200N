8bb52f6e99176948c36c560e1cbd4e4a135b283a
Awena SALIC <awena.salic@ens.uvsq.fr>
2022-04-15 09:50:59 +0200

    widgets + fenetre

--- essai1.py
+++ essai1.py
@@ -6,4 +6,37 @@
 #https://github.com/uvsq22101985/projetIN200N
 ######################
 
-jhbgc
+import tkinter as tk
+from turtle import width
+
+WIDTH = 800
+HEIGHT = 600
+
+###########################
+###FONCTIONS###
+###########################
+
+
+
+
+###########################
+racine = tk.Tk()
+racine.title("Puissance 4")
+
+canvas = tk.Canvas(racine, width=WIDTH, height= HEIGHT, bg = '#226D68')
+
+
+bouton_save = tk.Button(racine, text="Sauvegarder")
+bouton_annuler = tk.Button(racine, text="Annuler")
+bouton_icone1 = tk.Button(racine, text = "Joueur 1")
+bouton_icone2 = tk.Button(racine, text = "Joueur 2" )
+
+#placements des widgets
+bouton_save.grid(row =8, column = 1)
+bouton_annuler.grid(row = 8, column = 5)
+bouton_icone1.grid(row = 0, column = 1)
+bouton_icone2.grid(row = 0, column = 5)
+canvas.grid(row=1, column=1, columnspan=5)
+
+
+racine.mainloop()
