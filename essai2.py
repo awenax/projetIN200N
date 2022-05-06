5184bebdcbc80e8e7f358c8f82bdac912d8d4576
Awena SALIC <awena.salic@ens.uvsq.fr>
2022-05-06 17:57:29 +0200

    on peut bientot y jouer

--- essai2.py
+++ essai2.py
@@ -8,7 +8,8 @@
 
 # Variables
 import tkinter as tk
-from tkinter import Grid, ttk
+from tkinter import ttk
+import random
 
 WIDTH = 560
 HEIGHT = 560
@@ -17,15 +18,18 @@ hauteur_case = HEIGHT // 6
 color = "#18534F"
 player_1 = 1
 player_2 = 2
-grille = 
+grille = [[0] * 6 for i in range(7)]
+y = 0
 
 #############################
 # Fonctions
 def get_button(b):
+    global X
     X = b-1
     print(X)
 
-def set_grid(Grid[y][X]):
+def set_grid(grille):
+    global X, y
     """Fonction qui remets la grille à 0"""
     X, y = 0, 0
     while(grille[y][X]): 
@@ -36,16 +40,23 @@ def set_grid(Grid[y][X]):
         y +=1
     return(grille)
 
-def playable_grid(player_colour, X, grille[y][X]):
+def playable_grid(player_colour, X, grille):
+    global y
     """Fonction qui va lancer le jeu et verifier 
     dans la colonne choisie par le joueur
     s'il n'y a pas d'obstacle"""
     y = 5
-    while(grille[y][X]) != 0:
+    while(grille[y][X] != 0):
         y -=1
-    if(grille[y][X]) == 0:
+    if(grille[y][X] == 0):
         grille[y][X] = player_colour
     return y
+
+def player_begin():
+    global player_1, player_2
+    
+    
+
 
 #############################
 # Programme Principal #
@@ -62,7 +73,7 @@ for i in range(7):
                 ((i+1)*largeur_case, (j+1)*hauteur_case), fill=color)
 
 # Boutons
-bouton_setgrid = ttk.Button(racine, text = "Rejouer", command=set_grid(Grid[5][6]))
+bouton_setgrid = ttk.Button(racine, text = "Rejouer", command=set_grid(grille))
 bouton_column1 = ttk.Button(racine, text = "1", command=lambda:get_button(1))
 bouton_column2 = ttk.Button(racine, text = "2", command=lambda:get_button(2))
 bouton_column3 = ttk.Button(racine, text = "3", command=lambda:get_button(3))
@@ -71,6 +82,9 @@ bouton_column5 = ttk.Button(racine, text = "5", command=lambda:get_button(5))
 bouton_column6 = ttk.Button(racine, text = "6", command=lambda:get_button(6))
 bouton_column7 = ttk.Button(racine, text = "7", command=lambda:get_button(7))
 
+
+# Placements des widgets/boutons
+bouton_setgrid.grid(row = 8, column = 1, columnspan=1)
 bouton_column1.grid(row = 0, column = 1)
 bouton_column2.grid(row = 0, column = 2)
 bouton_column3.grid(row = 0, column = 3)
