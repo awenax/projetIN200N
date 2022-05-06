3cea0f2bfb5bc2033f871246a65855ffb060049d
Awena SALIC <awena.salic@ens.uvsq.fr>
2022-05-06 23:08:41 +0200

    nouveau

--- essai2.py
+++ essai2.py
@@ -5,9 +5,7 @@
 #SALIC Awena
 #https://github.com/uvsq22101985/projetIN200N
 ######################
-
 # Variables
-from pickle import NONE
 import tkinter as tk
 from tkinter import ttk
 import random
@@ -17,9 +15,10 @@ HEIGHT = 560
 largeur_case = WIDTH // 7
 hauteur_case = HEIGHT // 6
 color = "#18534F"
-grille = [[0] * 6 for i in range(7)]
+player_1 = 1
+player_2 = 2
+grille = [[0] * 7 for i in range(8)]
 grille_save = grille
-winner = NONE
 
 
 #############################
@@ -28,6 +27,7 @@ def get_button(b):
     global X
     X = b-1
     changement_joueur()
+    playable_grid(player_colour, X, grille)
     return X
 
 def set_grid(grille):
@@ -37,7 +37,7 @@ def set_grid(grille):
     while(grille[y][X]): 
         X = 0
         while(grille[y][X]):
-            grille[y][X] == 0
+            grille[y][X] = 0
             X += 1
         y +=1
     return(grille)
@@ -51,19 +51,36 @@ def playable_grid(player_colour, X, grille):
     while(grille[y][X] != 0):
         y -=1
     if(grille[y][X] == 0):
-        grille[y][X] = player_colour
-    return y 
+        grille[y][X] = canvas.create_oval(((largeur_case, hauteur_case),(largeur_case*2, hauteur_case*2)) , fill=player_colour)
+    return y
+
+def beginner_player():
+    global player_colour, player_turn
+    global beginner, choice_colour
+    couleurs = ["red", "yellow"]
+    choice_colour = random.choice(couleurs)
+    if choice_colour == "red":
+        player_turn = "Player 1"
+        player_colour = "red"
+    else:
+        player_turn = "Player 2"
+        player_colour = "yellow"
+    beginner = tk.Label(racine, text = player_turn, fg = player_colour, bg = "#535953")
+    beginner.grid(row=8, column = 3, columnspan=3)
+    
 
 def changement_joueur():
     global player_colour, player_turn
-    while winner == NONE:
-        if player_turn == "Player 1":
-            player_turn = "Player 2"
-            player_colour = "yellow"
-        else:
-            player_turn = "Player 1"
-            player_colour = "red"
-        beginner = tk.Label(racine, text = player_turn, fg = choice_colour, bg = "#535953")
+    global beginner
+    if player_turn == "Player 1":
+        player_turn = "Player 2"
+        player_colour = "yellow"
+    else:
+        player_turn = "Player 1"
+        player_colour = "red"
+    beginner = tk.Label(racine, text = player_turn, fg = player_colour, bg = "#535953")
+    beginner.grid(row=8, column = 3, columnspan=3)
+
         
 
 #############################
@@ -80,21 +97,8 @@ for i in range(7):
         rectangle = canvas.create_rectangle((i*largeur_case, j*hauteur_case),
                 ((i+1)*largeur_case, (j+1)*hauteur_case), fill=color)
 
-# Determination du joueur qui commence
-couleurs = ["red", "yellow"]
-choice_colour = random.choice(couleurs)
-if choice_colour == "red":
-    player_turn = "Player 1"
-    player_colour = "red"
-else:
-    player_turn = "Player 2"
-    player_colour = "yellow"
-
-beginner = tk.Label(racine, text = player_turn, fg = choice_colour, bg = "#535953")
-
-
 # Boutons
-bouton_setgrid = ttk.Button(racine, text = "Rejouer", command=set_grid(grille))
+bouton_setgrid = ttk.Button(racine, text = "Rejouer", command= set_grid(grille))
 bouton_column1 = ttk.Button(racine, text = "1", command=lambda:get_button(1))
 bouton_column2 = ttk.Button(racine, text = "2", command=lambda:get_button(2))
 bouton_column3 = ttk.Button(racine, text = "3", command=lambda:get_button(3))
@@ -113,8 +117,7 @@ bouton_column4.grid(row = 0, column = 4)
 bouton_column5.grid(row = 0, column = 5)
 bouton_column6.grid(row = 0, column = 6)
 bouton_column7.grid(row = 0, column = 7)
-beginner.grid(row=8, column = 3, columnspan=3)
 canvas.grid(row=1, column=1, columnspan=7)
 
-playable_grid(player_colour, X, grille)
+beginner_player()
 racine.mainloop()
