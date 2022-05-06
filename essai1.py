31a62a691149f1961dfa61f542e6da175e36c55f
Awena SALIC <awena.salic@ens.uvsq.fr>
2022-05-06 13:35:42 +0200

    clic jaune

--- essai1.py
+++ essai1.py
@@ -8,10 +8,10 @@
 
 import tkinter as tk
 
-WIDTH = 350
-HEIGHT = 350
-largeur_case = WIDTH // 5
-hauteur_case = HEIGHT // 7
+WIDTH = 560
+HEIGHT = 560
+largeur_case = WIDTH // 7
+hauteur_case = HEIGHT // 6
 color = "#18534F"
 
 
@@ -31,8 +31,8 @@ def souris(event):
     #mouvement()
 
 #def mouvement():
-    canvas.move(clic_rec, 0 , 1)
-    canvas.after(20, mouvement)
+    #canvas.move(clic_rec, 0 , 1)
+    #canvas.after(20, mouvement)
 
 #############################
 # Programme Principal #
@@ -44,8 +44,8 @@ canvas.bind('<Button-1>', souris)
 
 # Creation grille de jeu
 
-for i in range(5):
-    for j in range(7):
+for i in range(7):
+    for j in range(6):
         rectangle = canvas.create_rectangle((i*largeur_case, j*hauteur_case),
                 ((i+1)*largeur_case, (j+1)*hauteur_case), fill=color)
         
@@ -58,17 +58,21 @@ bouton_column2 = tk.Button(racine, text = "2")
 bouton_column3 = tk.Button(racine, text = "3")
 bouton_column4 = tk.Button(racine, text = "4")
 bouton_column5 = tk.Button(racine, text = "5")
+bouton_column6 = tk.Button(racine, text = "6")
+bouton_column7 = tk.Button(racine, text = "7")
 
 
 # Placements des widgets
 bouton_save.grid(row =8, column = 1)
-bouton_annuler.grid(row = 8, column = 5)
+bouton_annuler.grid(row = 8, column = 7)
 bouton_column1.grid(row = 0, column = 1)
 bouton_column2.grid(row = 0, column = 2)
 bouton_column3.grid(row = 0, column = 3)
 bouton_column4.grid(row = 0, column = 4)
 bouton_column5.grid(row = 0, column = 5)
-canvas.grid(row=1, column=1, columnspan=5)
+bouton_column6.grid(row = 0, column = 6)
+bouton_column7.grid(row = 0, column = 7)
+canvas.grid(row=1, column=1, columnspan=7)
 
 
 racine.mainloop()
